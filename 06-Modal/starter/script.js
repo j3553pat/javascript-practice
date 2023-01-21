5f7c2905211a28c47b4f0952fa7ca00b9c64081f
j3553pat <jessecodedev@gmail.com>
2023-01-21 14:08:49 -0600

    finished modal project

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -2,9 +2,30 @@
 
 const modal = document.querySelector('.modal')
 const overlay = document.querySelector('.overlay')
-const btn = document.querySelector('.close-modal')
+const btnCloseModal = document.querySelector('.close-modal')
 const btnsOpenModal = document.querySelectorAll('.show-modal')
 console.log(btnsOpenModal)
 
-for(let i = 0; i < btnsOpenModal.length; i++)
-console.log(btnsOpenModal[i].textContent)
+
+const openModal = function () {
+    modal.classList.remove('hidden')
+    modal.classList.remove('hidden')
+}
+
+const closeModal =  function() {
+    modal.classList.add('hidden')
+    overlay.classList.add('hidden')
+}
+
+btnCloseModal.addEventListener('click', closeModal)
+overlay.addEventListener('click', closeModal)
+
+for(let i = 0; i < btnsOpenModal.length; i++) 
+btnsOpenModal[i].addEventListener('click', openModal);
+
+document.addEventListener('keydown', function(event) {
+    console.log(event.key)
+if(event.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal()
+    }
+})
